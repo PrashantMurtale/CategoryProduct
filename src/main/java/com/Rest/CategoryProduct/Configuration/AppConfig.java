15bd1a8641e9e655bb976cd312d1ccf5258d673a
Prashant Murtale <88270338+PrashantMurtale@users.noreply.github.com>
2025-09-30 14:52:06 +0530

    Update AppConfig.java

--- src/main/java/com/Rest/CategoryProduct/Configuration/AppConfig.java
+++ src/main/java/com/Rest/CategoryProduct/Configuration/AppConfig.java
@@ -2,16 +2,14 @@ package com.Rest.CategoryProduct.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableMethodSecurity
 @Configuration
+@EnableMethodSecurity
 public class AppConfig {
 
     // Password encoder bean
@@ -20,18 +18,15 @@ public class AppConfig {
         return new BCryptPasswordEncoder();
     }
 
-    // AuthenticationManager bean
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
-        return builder.getAuthenticationManager();
-    }
-
     // Security configuration
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()   // disable CSRF for testing
-            .authorizeHttpRequests()
-            .anyRequest().permitAll();  // allow all requests
+        http
+            .csrf().disable() // Disable CSRF for testing
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/actuator/**").permitAll() // Explicitly allow Prometheus endpoint
+                .anyRequest().permitAll() // Allow all other requests
+            );
         return http.build();
     }
 }
